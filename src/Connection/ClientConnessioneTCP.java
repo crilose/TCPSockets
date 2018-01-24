b9d1f466dafdc3adc1bd3329920087fa738f9163
Cristiano <crilose99@gmail.com>
2018-01-24 17:07:07 +0100

    Classe di esecuzione del client modificata

--- src/Connection/ClientConnessioneTCP.java
+++ src/Connection/ClientConnessioneTCP.java
@@ -19,42 +19,10 @@ public class ClientConnessioneTCP {
      * @param args the command line arguments
      */
     public static void main(String[] args){
-        //oggetto da usare per realizzare la connessione TCP
-        Socket connection = null;
-        //nome o IP del server
-        String serverAddress = "localhost";
-        //porta del server in ascolto
-        int port = 2000;
-
-        //apertura della connessione al server sulla porta specificata
-        try{
-            connection = new Socket(serverAddress, port);
-            System.out.println("Connessione aperta");
-        }
-        catch(ConnectException e){
-            System.err.println("Server non disponibile!");
-        }
-        catch(UnknownHostException e1){
-            System.err.println("Errore DNS!");
-        }
-
-        catch(IOException e2){//
-            System.err.println(e2);
-            e2.printStackTrace();
-        }
-
-        //chiusura della connnessione
-        finally{
-                try {
-            if (connection!=null)
-                {
-                    connection.close();
-                    System.out.println("Connessione chiusa!");
-                }
-            }
-            catch(IOException e){
-                System.err.println("Errore nella chiusura della connessione!");
-            }
-        }
-    }
+        
+        ClientTCP client = new ClientTCP();
+        client.connessione();
+        client.comunica();
+        client.chiudiConnessione();
+}
 }
