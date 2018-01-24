55fa3e571edb3bf3e20986bead079caa2e6ac949
Cristiano <crilose99@gmail.com>
2018-01-24 17:07:15 +0100

    Classe di esecuzione del server modificata

--- src/Connection/ServerConnessioneTCP.java
+++ src/Connection/ServerConnessioneTCP.java
@@ -21,36 +21,10 @@ public class ServerConnessioneTCP {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // porta del server maggiore di 1024 
-        int port=2000;
-        //oggetto ServerSocket necessario per accettare richieste dal client
-        ServerSocket sSocket = null;
-        //oggetto da usare per realizzare la connessione TCP
-        Socket connection;
-        //blablabla
-
-        while(true){
-            try{
-                // il server si mette in ascolto sulla porta voluta
-                sSocket = new ServerSocket(port);
-                System.out.println("In attesa di connessioni!");
-                //si è stabilita la connessione
-                connection = sSocket.accept();
-                System.out.println("Connessione stabilita!");
-                System.out.println("Socket server: " + connection.getLocalSocketAddress());
-                System.out.println("Socket client: " + connection.getRemoteSocketAddress());
-            }
-               catch(IOException e){
-                   System.err.println("Errore di I/O!");
-            }
-            
-            //chiusura della connessione con il client
-            try {
-                if (sSocket!=null) sSocket.close();
-            } catch (IOException ex) {
-                System.err.println("Errore nella chiusura della connessione!");
-            }
-            System.out.println("Connessione chiusa!");
-        }
-      }
+        
+        ServerTCP server = new ServerTCP();
+        server.connessione();
+        server.comunica();
+        server.chiudiConnessione();
+}
 }
